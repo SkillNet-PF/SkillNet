26fcc5d49b2b77bff3e73e6687b90437436ac8c0
PauloCLG09 <pauce01@gmail.com>
2025-10-13 23:12:42 -0500

    cambios

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3002";
+const API_URL = "/api";
 
 export async function http<T>(path: string, options?: RequestInit): Promise<T> {
   const token = localStorage.getItem("accessToken");
